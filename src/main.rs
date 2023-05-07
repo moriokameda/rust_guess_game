98741103d2c6036e15b92ae37775cc4e78291330
moriokameda <mkame19941031@gmail.com>
2023-05-07 17:34:04 +0900

    add future

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::io;
+use std::future::Future;
+use futures::executor;
+
+async fn async_add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+async fn something_great_async_function() -> i32 {
+    let ans = async_add(2, 3).await;
+
+    println!("{}", ans);
+    ans
+}
+fn some_great_function() -> impl Future<Output = i32> {
+    async {
+        let value = 5;
+        send_to_another_thread_with_borrowing(&value).await
+    }
+}
+
+async fn send_to_another_thread_with_borrowing(x: &i32) -> i32 {
+    *x
+}
 
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
+    executor::block_on(something_great_async_function());
 }
